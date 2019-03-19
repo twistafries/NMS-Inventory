0885f2076c0694dbfa7143b4968ebf14d14a53f5
lovelynparis <2145338@slu.edu.ph>
2019-03-19 10:34:46 +0800

    #0 Added function for addinf equipment

--- finditwebsite/app/Models/TblItEquipment.php
+++ finditwebsite/app/Models/TblItEquipment.php
@@ -32,12 +32,19 @@ class TblItEquipment extends Model
         return $query;    
     }
     
-    public static function add_equipment($params = null){
-        $query = \DB::table('it_equipment')
-        -> where('type_id' , '=' , '3')
-        -> orderBy('created_at' , 'desc')
-        -> get();
-        return $query;    
+
+/*
+INSERT INTO `findit`.`it_equipment` (`type_id`, `name_or_model`, `details`, `serial_no`, `or_no`, `unit_id`, `status_id`) 
+VALUES ('1', 'Keyboard', 'Logitech', '897adP', '7984563', '2', '1'); 
+*/ 
+
+    public static function add_equipment($params){
+        $it_equipment = new TblItEquipment;
+        $it_equipment->type_id =  $params['type_id'];
+        $it_equipment->name_or_model = $params['name_or_model'];
+        $it_equipment->name_or_model = $params['details'];
+        $it_equipment->name_or_model = $params['serial_no'];
+        $it_equipment->name_or_model = $params['or_no'];
     }
 
     
