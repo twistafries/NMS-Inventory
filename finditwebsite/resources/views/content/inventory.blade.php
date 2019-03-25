8ffd6f54316738779ccf2f450b1e3b07c110384f
lovelynparis <2145338@slu.edu.ph>
2019-03-25 09:21:11 +0800

    Placed toolbar below tabs and added scripts

--- finditwebsite/resources/views/content/inventory.blade.php
+++ finditwebsite/resources/views/content/inventory.blade.php
@@ -18,70 +18,11 @@
 
 @section('content')
 <form action="" id="form1">
-    <!-- Toolbox -->
-    <div class="d-flex flex-row-reverse">
-        <div class="p-2">
-            <!-- Single Add Modal -->
-            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="ModalTitle">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                        </div>
-
-                        <div class="modal-body">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-info"> <span class="fas fa-plus"></span> ADD ITEM</button>
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">CANCEL</button>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn">
-                    <a href="#" data-toggle="tooltip" title="Multi Select">
-                        <img class="tool-item" src="{{ asset('assets/icons/table-toolbar-icons/checkbox-icon.png') }}"></a>
-                </button>
-                
-                <button type="button" class="btn">
-                    <a href="#" data-toggle="tooltip" title="Edit">
-                        <img class="tool-item" src="{{ asset('assets/icons/table-toolbar-icons/edit-icon.png') }}"></a>
-                </button>
-
-                <button type="button" class="btn">
-                    <a href="#" data-toggle="tooltip" title="Hide/Unhide">
-                    <img class="tool-item"  src="{{ asset('assets/icons/table-toolbar-icons/view.png') }}"></a>
-                </button>
-                <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href="#" data-toggle="tooltip" title="Add"><img class="tool-item"  src="../../assets/icons/table-toolbar-icons/add-icon.png"></a>
-            </button>
-                <button type="button" class="btn">
-                    <a href="#" data-toggle="tooltip" title="delete"> 
-                        <img class="tool-item"  src="../../assets/icons/table-toolbar-icons/delete-icon.png"></a>
-                    </button>
-                <button type="button" class="btn">
-                    <a href="#" data-toggle="tooltip" title="sort">
-                        <img class="tool-item"  src="../../assets/icons/table-toolbar-icons/sort-icon.png"></a>
-                    </button>
-
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Add (5 items or Less)</a>
-                    <a class="dropdown-item" href="#">Bulk Add</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Tabs -->
+    
     <div class="container">
-		<ul class="nav nav-pills mb-3 p-3 nav-justified nav-fill font-weight-bold" id="pills-tab" role="tablist">
+        
+        <!-- Tabs -->
+		<ul class="nav nav-pills p-3 nav-justified nav-fill font-weight-bold" id="pills-tab" role="tablist">
 			<li class="nav-item text-uppercase">
 				<a class="nav-link active " id="pills-0-tab" data-toggle="pill" href="#pills-0" role="tab" aria-controls="pills-0" aria-selected="true">
                     All</a>
@@ -106,14 +47,94 @@
 			<li class="nav-item text-uppercase">
 				<a class="nav-link" id="pills-4-tab" data-toggle="pill" href="#pills-4" role="tab" aria-controls="pills-4" aria-selected="false">tab 4</a>
 			</li> -->
-		</ul>
+        </ul>
+        
+        <!-- Toolbox -->
+        <div class="d-flex flex-row-reverse">
+            <div class="p-2">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <!-- Multiple Select -->
+                    <button type="button" class="btn" id="multiple-select">
+                        <a href="#" data-toggle="tooltip" title="Multiple Select">
+                            <img class="tool-item" src="{{ asset('assets/icons/table-toolbar-icons/checkbox-icon.png') }}">
+                        </a>
+                    </button>
+                    
+                    <!-- Edit -->
+                    <button type="button" class="btn" id="edit">
+                        <a href="#" data-toggle="tooltip" title="Edit">
+                            <img class="tool-item" src="{{ asset('assets/icons/table-toolbar-icons/edit-icon.png') }}">
+                        </a>
+                    </button>
+    
+                    <!-- Hide/Unhide Column -->
+                    <button type="button" class="btn hide-column">
+                        <a href="#" data-toggle="tooltip" title="Hide/Unhide">
+                            <img class="tool-item"  src="{{ asset('assets/icons/table-toolbar-icons/view.png') }}">
+                        </a>
+                    </button>
+    
+                    <!-- Add -->
+                    <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a href="#" data-toggle="tooltip" title="Add">
+                            <img class="tool-item"  src="../../assets/icons/table-toolbar-icons/add-icon.png">
+                        </a>
+                    </button>
+                    
+                    <!-- Delete -->
+                    <button type="button" class="btn disabled">
+                        <a href="#" data-toggle="tooltip" title="delete"> 
+                            <img class="tool-item"  src="../../assets/icons/table-toolbar-icons/delete-icon.png">
+                        </a>
+                        </button>
+    
+                    <!-- Sort -->
+                    <!-- <button type="button" class="btn">
+                        <a href="#" data-toggle="tooltip" title="sort">
+                            <img class="tool-item"  src="../../assets/icons/table-toolbar-icons/sort-icon.png">
+                        </a>
+                    </button> -->
+    
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Add (5 items or Less)</a>
+                        <a class="dropdown-item" href="#">Bulk Add</a>
+                    </div>
+                </div>
+    
+                <!-- Single Add Modal -->
+                <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="ModalTitle">Modal title</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                            </div>
+    
+                            <div class="modal-body">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            </div>
+    
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-info"> <span class="fas fa-plus"></span> ADD ITEM</button>
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">CANCEL</button>
+    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tab Content -->
 		<div class="tab-content" id="pills-tabContent">
             <!-- All Items in the Inventory -->
 			<div class="tab-pane fade show active" id="pills-0" role="tabpanel" aria-labelledby="pills-0-tab">
-                <table id="myDataTable" class="table table-borderless table-hover" style="width:100%">
+                <table id="myDataTable" class="table table-borderless table-striped table-hover" style="width:100%">
                     <thead class="thead-dark">
                         <tr>
-
+                            <th></th>
                             <th>Name</th>
                             <th>Details</th>
                             <th>Serial No</th>
@@ -127,7 +148,7 @@
 
                         @foreach ($equipment as $equipment)
                         <tr>
-
+                            <td></td>
                             <td> {{ $equipment->name }} </td>
                             <td width="30%"> {{ $equipment->details }} </td>
                             <td> {{ $equipment->serial_no }} </td>
@@ -179,39 +200,38 @@
             </div>
 
             <div class="tab-pane fade" id="pills-2" role="tabpanel" aria-labelledby="pills-2-tab">
-                      <table id="myDataTable2" class="table table-borderless table-hover" style="width:100%">
-                          <thead class="thead-dark">
-                              <tr>
+                <table id="myDataTable2" class="table table-borderless table-hover" style="width:100%">
+                    <thead class="thead-dark">
+                        <tr>
 
-                                  <th>Name</th>
-                                  <th>Details</th>
-                                  <th>Serial No</th>
-                                  <th>OR No</th>
-                                  <th>Added At</th>
-                                  <th width="15%">Edited At</th>
-                                  <th>Status</th>
-                              </tr>
-                          </thead>
-                          <tbody>
+                            <th>Name</th>
+                            <th>Details</th>
+                            <th>Serial No</th>
+                            <th>OR No</th>
+                            <th>Added At</th>
+                            <th width="15%">Edited At</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-                              @foreach ($peripherals as $peripherals)
-                              <tr>
+                        @foreach ($peripherals as $peripherals)
+                        <tr>
 
-                                  <td> {{ $peripherals->name }} </td>
-                                  <td width="30%"> {{ $peripherals->details }} </td>
-                                  <td> {{ $peripherals->serial_no }} </td>
-                                  <td> {{ $peripherals->or_no }} </td>
-                                  <td> {{ $peripherals->created_at }} </td>
-                                  <td> {{ $peripherals->updated_at }} </td>
-                                  <td> {{ $peripherals->stat }} </td>
-                              </tr>
+                            <td> {{ $peripherals->name }} </td>
+                            <td width="30%"> {{ $peripherals->details }} </td>
+                            <td> {{ $peripherals->serial_no }} </td>
+                            <td> {{ $peripherals->or_no }} </td>
+                            <td> {{ $peripherals->created_at }} </td>
+                            <td> {{ $peripherals->updated_at }} </td>
+                            <td> {{ $peripherals->stat }} </td>
+                        </tr>
 
-                              @endforeach
-                          </tbody>
+                        @endforeach
+                    </tbody>
 
-                      </table>
-                  </div>
-
+                </table>
+            </div>
 
             <!-- Mobile Devices -->
 			<div class="tab-pane fade" id="pills-3" role="tabpanel" aria-labelledby="pills-3-tab">
@@ -248,8 +268,6 @@
                 </table>
             </div>
 
-            </div>
-
 		</div>
 	</div>
 
@@ -258,9 +276,10 @@
 </form>
 @stop
 
-@section('script')
 
+    @section('script')
     <!-- Datatable -->
+     <!-- Datatable -->
     <script type="text/javascript" src="{{ asset('js/datatable/jquery.dataTables.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/datatable/datatables.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/datatable/dataTables.bootstrap4.min.js') }}"></script>
@@ -270,7 +289,7 @@
 
     <!-- Additional Scripts   -->
     <script type="text/javascript" src="{{ asset('js/datatable/dataTables.select.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/datatable/dataTables.checkboxes.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/datatable/dataTables.checkboxes.min.js') }}"></script> 
 
     <script>
       $(document).ready(function(){
@@ -279,10 +298,10 @@
     </script>
     <script type="text/javascript">
     $(document).ready(function() {
-        $('table').DataTable({
+        var table = $('table').DataTable({
             scrollY: '50vh',
             scrollCollapse: true,
-            scrollX: '50vw',
+            // scrollX: '100vw',        
             
             'ajax': '',
             'select': 'multi',
@@ -307,22 +326,14 @@
             }]
  
         });
+
+        $('a[data-toggle="pill"]').on('show.bs.tab' , function(e){
+            console.log("inside tab fn");
+            $($.fn.dataTable.tables(true)).DataTable().scroller.measure();
+        });
+        console.log("skipped");
     } );
     </script>
-    <!-- <script type="text/javascript">
-    $(document).ready(function() {
-        $('#myDataTable1').DataTable();
-    } );
-    </script> -->
-    <script type="text/javascript">
-    $(document).ready(function() {
-        $('#myDataTable2').DataTable();
-    } );
-    </script>
-    <script type="text/javascript">
-    $(document).ready(function() {
-        $('#myDataTable3').DataTable();
-    } );
-    </script>
+    
 
 @stop
