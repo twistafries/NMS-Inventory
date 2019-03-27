dee65a94d7b385a97b987f2458afc00b30fad523
lovelynparis <2145338@slu.edu.ph>
2019-03-28 00:54:57 +0800

    Added functions for pending and available statuses

--- finditwebsite/app/Models/TblStatus.php
+++ finditwebsite/app/Models/TblStatus.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class TblStatus extends Model
 {
+    public static function get_all_equipment_status($params = null){
+        $query = \DB::table('equipment_status')
+        -> orderBy('name' , 'asc')
+        -> get();
+        // dd($query);
+        return $query;
+    }
     
+    public static function get_pending_and_available_status($params = null){
+        $query = \DB::table('equipment_status')
+        -> orderBy('name' , 'asc')
+        -> where('id' , '=' , '1')
+        -> where('id' , '=' , '6')
+        -> get();
+        // dd($query);
+        return $query;
+    }
 }
